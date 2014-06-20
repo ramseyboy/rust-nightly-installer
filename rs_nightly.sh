2a78f9e85295a255aeb811dd47ff3024b04af13f
ramseyboy <walker.hannan@willowtreeapps.com>
2014-06-19 22:15:18 -0400

    added license header to script

--- rs_nightly.sh
+++ rs_nightly.sh
@@ -2,6 +2,19 @@
 #downloads and installs nightly rust release on OSX
 #installer puts rust binaries in /usr/local/bin by default
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 readonly pkg_prefix=rust-nightly
 readonly rust_pkg_name="${pkg_prefix}-`eval date +%m-%d-%Y`.pkg"
 
